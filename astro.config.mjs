f99becdcf79c895335195a9b01f2b94df974387d
Giulio Zanchetta <zank@outlook.it>
2023-09-07 00:27:52 +0200

    Init

---
+++ astro.config.mjs
@@ -0,0 +1,95 @@
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import NetlifyCMS from "astro-netlify-cms";
+import astroI18next from "astro-i18next";
+import alpinejs from "@astrojs/alpinejs";
+import AstroPWA from "@vite-pwa/astro";
+
+// https://astro.build/config
+export default defineConfig({
+	site: "https://saasophonic.zank.studio",
+	vite: {
+		define: {
+			__DATE__: `'${new Date().toISOString()}'`,
+		},
+	},
+	integrations: [
+		tailwind(),
+		mdx(),
+		sitemap(),
+		NetlifyCMS({
+			config: {
+				backend: {
+					name: "github",
+					repo: "zankhq/saasophonic",
+					branch: "main",
+					base_url: "https://saasophonic.zank.studio",
+					auth_endpoint: "/api/auth",
+				},
+				media_folder: "public/images",
+				public_folder: "/images",
+				i18n: {
+					structure: "multiple_folders",
+					locales: ["en", "it"],
+					default_locale: "en",
+				},
+				collections: [
+					// Content collections
+					{
+						name: "posts",
+						i18n: true,
+						label: "Blog Posts",
+						folder: "src/content/blog",
+						create: true,
+						delete: true,
+						fields: [],
+					},
+				],
+			},
+			disableIdentityWidgetInjection: true,
+		}),
+		astroI18next(),
+		alpinejs(),
+		AstroPWA({
+			mode: "production",
+			base: "/",
+			scope: "/",
+			includeAssets: ["favicon.svg"],
+			registerType: "autoUpdate",
+			manifest: {
+				name: "Astro starter with tailwind, alpine and i18n support.",
+				short_name: "Astro starter",
+				theme_color: "#ffffff",
+				icons: [
+					{
+						src: "pwa-192x192.png",
+						sizes: "192x192",
+						type: "image/png",
+					},
+					{
+						src: "pwa-512x512.png",
+						sizes: "512x512",
+						type: "image/png",
+					},
+					{
+						src: "pwa-512x512.png",
+						sizes: "512x512",
+						type: "image/png",
+						purpose: "any maskable",
+					},
+				],
+			},
+			workbox: {
+				navigateFallback: "/404",
+				globPatterns: ["*.js"],
+			},
+			devOptions: {
+				enabled: true,
+				navigateFallbackAllowlist: [/^\/404$/],
+				suppressWarnings: true,
+			},
+		}),
+	],
+});
